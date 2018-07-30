29b5449dc4ff0b71d00db45b3298fab63386de93
leedaeho <eogh4535@naver.com>
2018-07-30 19:49:41 +0900

    cleaar

--- TourTogether/src/main/java/com/cndy/tt/member/MemberDao.java
+++
@@ -1,8 +0,0 @@
-package com.cndy.tt.member;
-
-public interface MemberDao {
-	void insert(Member member);
-	long checkId(Member member); 
-	Member profileContent(String id);
-	long profileUpdate(Member member);
-}
