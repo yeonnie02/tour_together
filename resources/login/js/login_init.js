e0cda37f1d6ea63beb835ecb32833b1a5126f6f9
yeonnie02 <yeonnie02@gmail.com>
2018-09-13 11:13:57 +0900

    no message

--- resources/login/js/login_init.js
+++
@@ -1,7 +0,0 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAHO8BfrJxutBbFw8Af7p0GnZDcOMlwl8A",
-    authDomain: "tourtogether-webpj01.firebaseapp.com",
-    projectId: "tourtogether-webpj01",
-  };
-  firebase.initializeApp(config);
