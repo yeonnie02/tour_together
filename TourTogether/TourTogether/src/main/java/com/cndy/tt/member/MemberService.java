793db9f033700dd15752ac8603509c1f803ce94f
yeonnie02 <yeonnie02@gmail.com>
2018-09-13 11:25:28 +0900

    no message

--- TourTogether/TourTogether/src/main/java/com/cndy/tt/member/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.cndy.tt.member;
-
-public interface MemberService {
-	boolean insertService(Member member);
-	long checkIdService(Member member);
-	Member profileContentService(String id);
-	long updateService(Member member);
-	String checkAuthorityService(String id);
-}
